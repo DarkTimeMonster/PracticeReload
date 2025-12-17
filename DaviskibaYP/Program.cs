08b30ac656ffcd7f14f05725c3fd08d4ae2447ec
Prime <dawiskibamaksim@gmail.com>
2025-12-17 15:37:37 +0200

    program feat: add test message

--- DaviskibaYP/Program.cs
+++ DaviskibaYP/Program.cs
@@ -125,3 +125,6 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
+//test message
--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18fa87f2dcbaba6d0e15501ce8918a361aac67b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b7a0a902090657fe8cf7f1f6983a2dbd773dc21")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
